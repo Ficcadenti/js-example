eee315b9bea818360a8ddd1275999437fca29dc1
Raffaele Ficcadenti <raffaele.ficcadenti@gmail.com>
2016-10-28 15:50:19 +0200

    Commit iniziale

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+  $path_sviluppo="./";
+?>
+<style>
+	.disabled {
+   pointer-events: none;
+   cursor: default;
+}
+</style>
+<h2><strong> Repository per Lezioni/esempi Java Script </strong></h2>
+(c) 09/2016 - Raffaele Ficcadenti (raffaele.ficcadenti@gmail.com) <br>
+Ho cercato di raccogliere in questo repository, le basi della programmazione Java Script.
+Per ogni correzione o suggerimento, non esitate a scrivermi.
+Buon 'coding'.
+Raffaele.
+<p>
+	<b>Prima di partire</b><br><br>
+	Per gli esempi che utilizzano chiamate a MySQL dovrete installare il database di esempio con le relative tabelle,<br>
+	ovviamente deve essere presente un'installazione di MySQL, e dovete conoscere la password di root.<br><br>
+	Lanciare da riga di comando:<br><br>
+		mysql -u <strong>root</strong> -p{<strong>password</strong>} -h {<strong>nomehost</strong>} < <strong>php-example.sql</strong><br><br>
+	Lo script <strong>php-example.sql</strong> lo trovate versionato sotto la directory dumpdb.
+</p>
+<ul>
+	<li><strong>LEZIONE N. 0</strong> - Per iniziare.
+		<ul>
+			<br>
+			<li><b><a href="<?php echo $path_sviluppo?>Lazione-00 - Per iniziare/esempio_01.html" target="_blank">esempio_01.html</a></b>: Un semplice script periniziare.</li>
+			<br>
+		</ul>
+	</li>
+</ul>
