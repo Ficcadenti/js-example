692919b27d1bfa3b661bafab148b18a54702d053
Raffaele Ficcadenti <raffaele.ficcadenti@gmail.com>
2017-04-03 19:04:24 +0200

    AngularJS - Filtri

--- APPENDICE/01 - Angular/assets/js/controller_es04.js
+++ APPENDICE/01 - Angular/assets/js/controller_es04.js
@@ -2,4 +2,20 @@ function AppCtrl($scope)
 {
 	$scope.config={"appname": "Esempio 04 - I Filtri"};
 	//$scope.config.appname="Esempio 04 - I Filtri";
+	$scope.missioni = [{
+			nome: 'Swift',
+            lancio: '743634345123'
+        },
+        {
+        	nome: 'Nustar',
+            lancio: '943634345438'
+        },
+        {
+        	nome: 'Fermi',
+            lancio: '643634245436'
+        },
+        {
+        	nome: 'Pamela',
+            lancio: '593134345436'
+        }];
 }
